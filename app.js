18b7efd4eae62295f1e0bb91b0b0371b8df43c58
Dadams11 <adams.1743@osu.edu>
2023-09-18 23:12:22 -0400

    added vite to the project

---
+++ app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Header from './React_Portfolio/src/components/Header';
+import Footer from './React_Portfolio/src/components/Footer';
+import AboutMe from './React_Portfolio/src/pages/AboutMe';
+import Portfolio from './React_Portfolio/src/pages/Portfolio';
+import Contact from './React_Portfolio/src/pages/Contact';
+import Resume from './React_Portfolio/src/pages/Resume';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/" component={AboutMe} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
