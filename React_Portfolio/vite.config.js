2a6fb7c0cf88b6ec7279c86875dd36e70a42b49c
Dadams11 <adams.1743@osu.edu>
2023-09-25 20:29:06 -0400

    cleaned up code and files

--- React_Portfolio/vite.config.js
+++ React_Portfolio/vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'automatic' })], // Specify the automatic JSX runtime
+  plugins: [react({ jsxRuntime: 'automatic' })], 
   server: {
     port: 3001,
     open: true
   }
-  // No need for the esbuild configuration for JSX, 
-  // as the automatic runtime handles this now.
 })
