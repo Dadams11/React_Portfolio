4f3c4e31298c8865fcd2a30ab42d22e486e9f255
Dadams11 <adams.1743@osu.edu>
2023-09-25 20:01:14 -0400

    updated color scheme

--- React_Portfolio/src/App.jsx
+++ React_Portfolio/src/App.jsx
@@ -1,75 +1,76 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';  // Route added here
 import Navigation from './components/Navigation';
+import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <header>
-          <h1>My Portfolio</h1>
-          <Navigation />
-        </header>
+    return (
+        <Router>
+            <div className="container">
+                <header className="app-header">
+                    <h1>Darnell's Portfolio</h1>
+                    <Navigation />
+                </header>
 
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <AboutMe />
-            </Route>
-            <Route path="/portfolio">
-              <Portfolio />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/resume">
-              <Resume />
-            </Route>
-          </Switch>
-        </main>
+                <main className="app-main">
+                    <Switch>
+                        <Route exact path="/">
+                            <AboutMe />
+                        </Route>
+                        <Route path="/portfolio">
+                            <Portfolio />
+                        </Route>
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route path="/resume">
+                            <Resume />
+                        </Route>
+                    </Switch>
+                </main>
 
-        <footer>
-          <p>Find me on GitHub, LinkedIn, and Twitter.</p>
-        </footer>
-      </div>
-    </Router>
-  );
+                <footer className="app-footer">
+                    <p>Find me on GitHub, LinkedIn, and Twitter.</p>
+                </footer>
+            </div>
+        </Router>
+    );
 }
 
 function AboutMe() {
-  return (
-    <section id="aboutme">
-      <h2>About Me</h2>
-      <p>This is the about me section.</p>
-    </section>
-  );
+    return (
+        <section id="aboutme">
+            <h2>About Me</h2>
+            <p>This is the about me section.</p>
+        </section>
+    );
 }
 
 function Portfolio() {
-  return (
-    <section id="portfolio">
-      <h2>Portfolio</h2>
-      <p>This is the portfolio section.</p>
-    </section>
-  );
+    return (
+        <section id="portfolio">
+            <h2>Portfolio</h2>
+            <p>This is the portfolio section.</p>
+        </section>
+    );
 }
 
 function Contact() {
-  return (
-    <section id="contact">
-      <h2>Contact</h2>
-      <p>This is the contact section.</p>
-    </section>
-  );
+    return (
+        <section id="contact">
+            <h2>Contact</h2>
+            <p>This is the contact section.</p>
+        </section>
+    );
 }
 
 function Resume() {
-  return (
-    <section id="resume">
-      <h2>Resume</h2>
-      <p>This is the resume section.</p>
-    </section>
-  );
+    return (
+        <section id="resume">
+            <h2>Resume</h2>
+            <p>This is the resume section.</p>
+        </section>
+    );
 }
 
 export default App;
